5bfb4e617cf333a1b65100680694f42a3a94ffcd
Gnomorian <gnomorian@gmail.com>
2014-11-02 05:35:27 +1300

    first attempt at growing plants on custom farmland

--- src/main/java/nz/co/crookedhill/ggutils/block/GrowthBlock.java
+++ src/main/java/nz/co/crookedhill/ggutils/block/GrowthBlock.java
@@ -3,6 +3,9 @@ package nz.co.crookedhill.ggutils.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.world.World;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class GrowthBlock extends Block {
 	
@@ -14,5 +17,9 @@ public class GrowthBlock extends Block {
 		this.setCreativeTab(CreativeTabs.tabMisc);
 		//this.setBlockTextureName("ggutils:GrowthBlock"); no texture yet
 		
+	}
+	
+	public boolean canSustainPlant(World world, int x, int y, int z, ForgeDirection direction, IPlantable plantable) {
+		return true;
 	}
 }
