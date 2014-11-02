f7138534536feccb6c2f7b74cf627ddb36af0a59
Gnomorian <gnomorian@gmail.com>
2014-11-02 18:40:02 +1300

    Sortivator Inventory on top on activating the sortivator it opens the inventory of the chest sitting ontop of the sortivator, its just a proof of concept. it is done with 3 functions taken from the BlockChest Class which have screwed up parameters and method names.

--- src/main/java/nz/co/crookedhill/ggutils/block/Sortivator.java
+++ src/main/java/nz/co/crookedhill/ggutils/block/Sortivator.java
@@ -1,12 +1,23 @@
 package nz.co.crookedhill.ggutils.block;
 
+import static net.minecraftforge.common.util.ForgeDirection.DOWN;
+
+import java.util.Iterator;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntityOcelot;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryLargeChest;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import nz.co.crookedhill.ggutils.GGUtils;
@@ -46,5 +57,102 @@ public class Sortivator extends Block {
 		System.out.println("Entitie's: "+((EntityPlayer)player).cameraYaw);
 		System.out.println("ItemStack: "+item.getDisplayName());
 	}	
+	@Override
+	public boolean onBlockActivated(World p_149727_1_, int p_149727_2_, int p_149727_3_, int p_149727_4_, EntityPlayer p_149727_5_, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
+    {
+        if (p_149727_1_.isRemote)
+        {
+            return true;
+        }
+        else
+        {
+            IInventory iinventory = this.func_149951_m(p_149727_1_, p_149727_2_, p_149727_3_+1, p_149727_4_);
+
+            if (iinventory != null)
+            {
+                p_149727_5_.displayGUIChest(iinventory);
+            }
+
+            return true;
+        }
+    }
+	//gets an inventory object from the coordernates specified
+	public IInventory func_149951_m(World p_149951_1_, int p_149951_2_, int p_149951_3_, int p_149951_4_)
+    {
+        Object object = (TileEntityChest)p_149951_1_.getTileEntity(p_149951_2_, p_149951_3_, p_149951_4_);
+
+        if (object == null)
+        {
+            return null;
+        }
+        else if (p_149951_1_.isSideSolid(p_149951_2_, p_149951_3_ + 1, p_149951_4_, DOWN))
+        {
+            return null;
+        }
+        else if (func_149953_o(p_149951_1_, p_149951_2_, p_149951_3_, p_149951_4_))
+        {
+            return null;
+        }
+        else if (p_149951_1_.getBlock(p_149951_2_ - 1, p_149951_3_, p_149951_4_) == this && (p_149951_1_.isSideSolid(p_149951_2_ - 1, p_149951_3_ + 1, p_149951_4_, DOWN) || func_149953_o(p_149951_1_, p_149951_2_ - 1, p_149951_3_, p_149951_4_)))
+        {
+            return null;
+        }
+        else if (p_149951_1_.getBlock(p_149951_2_ + 1, p_149951_3_, p_149951_4_) == this && (p_149951_1_.isSideSolid(p_149951_2_ + 1, p_149951_3_ + 1, p_149951_4_, DOWN) || func_149953_o(p_149951_1_, p_149951_2_ + 1, p_149951_3_, p_149951_4_)))
+        {
+            return null;
+        }
+        else if (p_149951_1_.getBlock(p_149951_2_, p_149951_3_, p_149951_4_ - 1) == this && (p_149951_1_.isSideSolid(p_149951_2_, p_149951_3_ + 1, p_149951_4_ - 1, DOWN) || func_149953_o(p_149951_1_, p_149951_2_, p_149951_3_, p_149951_4_ - 1)))
+        {
+            return null;
+        }
+        else if (p_149951_1_.getBlock(p_149951_2_, p_149951_3_, p_149951_4_ + 1) == this && (p_149951_1_.isSideSolid(p_149951_2_, p_149951_3_ + 1, p_149951_4_ + 1, DOWN) || func_149953_o(p_149951_1_, p_149951_2_, p_149951_3_, p_149951_4_ + 1)))
+        {
+            return null;
+        }
+        else
+        {
+            if (p_149951_1_.getBlock(p_149951_2_ - 1, p_149951_3_, p_149951_4_) == this)
+            {
+                object = new InventoryLargeChest("container.chestDouble", (TileEntityChest)p_149951_1_.getTileEntity(p_149951_2_ - 1, p_149951_3_, p_149951_4_), (IInventory)object);
+            }
+
+            if (p_149951_1_.getBlock(p_149951_2_ + 1, p_149951_3_, p_149951_4_) == this)
+            {
+                object = new InventoryLargeChest("container.chestDouble", (IInventory)object, (TileEntityChest)p_149951_1_.getTileEntity(p_149951_2_ + 1, p_149951_3_, p_149951_4_));
+            }
+
+            if (p_149951_1_.getBlock(p_149951_2_, p_149951_3_, p_149951_4_ - 1) == this)
+            {
+                object = new InventoryLargeChest("container.chestDouble", (TileEntityChest)p_149951_1_.getTileEntity(p_149951_2_, p_149951_3_, p_149951_4_ - 1), (IInventory)object);
+            }
+
+            if (p_149951_1_.getBlock(p_149951_2_, p_149951_3_, p_149951_4_ + 1) == this)
+            {
+                object = new InventoryLargeChest("container.chestDouble", (IInventory)object, (TileEntityChest)p_149951_1_.getTileEntity(p_149951_2_, p_149951_3_, p_149951_4_ + 1));
+            }
+
+            return (IInventory)object;
+        }
+    }
+	
+	private static boolean func_149953_o(World p_149953_0_, int p_149953_1_, int p_149953_2_, int p_149953_3_)
+    {
+        Iterator iterator = p_149953_0_.getEntitiesWithinAABB(EntityOcelot.class, AxisAlignedBB.getBoundingBox((double)p_149953_1_, (double)(p_149953_2_ + 1), (double)p_149953_3_, (double)(p_149953_1_ + 1), (double)(p_149953_2_ + 2), (double)(p_149953_3_ + 1))).iterator();
+        EntityOcelot entityocelot;
+
+        do
+        {
+            if (!iterator.hasNext())
+            {
+                return false;
+            }
+
+            Entity entity = (Entity)iterator.next();
+            entityocelot = (EntityOcelot)entity;
+        }
+        while (!entityocelot.isSitting());
+
+        return true;
+    }
 	
 }
