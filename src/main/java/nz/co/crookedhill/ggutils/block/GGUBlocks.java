38d76a13dfe800ec37275199457c8c1a556f17c1
Gnomorian <gnomorian@gmail.com>
2014-11-04 23:30:12 +1300

    cleaned up recipes after fixing it (bloody java and your ''"") also edited the tooltip of the growthblock so its less looooong

--- src/main/java/nz/co/crookedhill/ggutils/block/GGUBlocks.java
+++ src/main/java/nz/co/crookedhill/ggutils/block/GGUBlocks.java
@@ -26,20 +26,8 @@ public class GGUBlocks {
 		//register Block Recipes
 
 		//change speckled melon to bonemeal, cant find it in the list of items
-		
-		/**
-		 * having trouble with these recipes, they both are valid to eclipse, but when running they both crash.
-		 * iv tried running it in init() and the block registration in preint but to no avail either.
-		 */
-		
-		/*  RECIPE OPTION 1
 		GameRegistry.addRecipe(new ItemStack(GGUBlocks.growthBlock), "ddd", "isi","iii",
-		        'd', Blocks.dirt, 'i', Items.iron_ingot, "s", Items.speckled_melon);
-		*/
-		
-		/*  RECIPE OPTION 2 (adapted from shaoks code)
-		GameRegistry.addRecipe(new ItemStack(GGUBlocks.growthBlock), 
-				new Object[]{"ddd","isi","iii",'d', Blocks.dirt, 'i', Items.iron_ingot, "s", Items.speckled_melon});
-				*/
+		        'd', Blocks.dirt, 'i', Items.iron_ingot, 's', Items.speckled_melon);
+				
 	}
 }
--- src/main/java/nz/co/crookedhill/ggutils/handlers/GGUEventHandler.java
+++ src/main/java/nz/co/crookedhill/ggutils/handlers/GGUEventHandler.java
@@ -9,8 +9,10 @@ public class GGUEventHandler {
 	@SubscribeEvent
 	public void onTooltipEvent(ItemTooltipEvent event) {
 		if(event.itemStack.getItem().getUnlocalizedName().equals("tile.GrowthBlock")) {
-			event.toolTip.add("Growing plants has never been faster!");
-			event.toolTip.add("Stack these blocks up to "+GGUConfigManager.GrowthBlockStackHeight+" times to grow faster");
+			event.toolTip.add("Growing plants has");
+			event.toolTip.add("never been faster!");
+			event.toolTip.add("Stack these blocks up to ");
+			event.toolTip.add(GGUConfigManager.GrowthBlockStackHeight+" times to grow faster");
 		}
 		if(event.itemStack.getItem().getUnlocalizedName().equals("tile.sortivator")) {
 			event.toolTip.add("Right clicking on the the sortivator");
