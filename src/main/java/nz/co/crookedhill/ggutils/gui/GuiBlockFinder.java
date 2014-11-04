48bb6eee626ec8a67981a440a02cfa30b439e506
Unrelentless <p.boryseiko@gmail.com>
2014-11-04 17:45:11 +1100

    Gui Stuff.

--- src/main/java/nz/co/crookedhill/ggutils/gui/GuiBlockFinder.java
+++ src/main/java/nz/co/crookedhill/ggutils/gui/GuiBlockFinder.java
@@ -1,31 +1,32 @@
 package nz.co.crookedhill.ggutils.gui;
 
-import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.ContainerPlayer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.ResourceLocation;
 import nz.co.crookedhill.ggutils.GGUtils;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly (Side.CLIENT)
-public class GuiBlockFinder extends GuiContainer {
+public class GuiBlockFinder extends GuiScreen {
 
-	ResourceLocation textureLoc = new ResourceLocation(GGUtils.MODID + ":" + "textures/gui/GUIBlockFinder"+".png");
+	private ResourceLocation textureLoc = new ResourceLocation(GGUtils.MODID + ":" + "textures/gui/GUIBlockFinder"+".png");
+	private Minecraft mc;
 	
-	public GuiBlockFinder(EntityPlayer player, InventoryPlayer inventoryPlayer){
-		super(new ContainerPlayer(inventoryPlayer, true, player));
+	public GuiBlockFinder(Minecraft mc)
+	{
+		this.mc = mc;
 	}
 	
 	@Override
-	public void drawScreen(int x, int y, float f) {
+	public void initGui() {
 		// TODO Auto-generated method stub
-		super.drawScreen(x, y, f);
+		super.initGui();
 	}
-
+	
 	@Override
-	protected void drawGuiContainerBackgroundLayer(float f, int x, int y) {
-		
+	public void drawScreen(int x, int y, float z) {
+		// TODO Auto-generated method stub
+		super.drawScreen(x, y, z);
 	}
 }
