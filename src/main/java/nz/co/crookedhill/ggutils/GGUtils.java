bb1eeb7ccedad3dc7acce92e6fa41f9a5aec91da
Gnomorian <gnomorian@gmail.com>
2014-11-22 02:59:51 +1300

    [version] there hasnt be 4 bugfixes since the fact

--- src/main/java/nz/co/crookedhill/ggutils/GGUtils.java
+++ src/main/java/nz/co/crookedhill/ggutils/GGUtils.java
@@ -68,7 +68,7 @@ public class GGUtils
 	 * forth 0=
 	 * 	the number of bug fixes/sub features added since last feature added.
 	 */
-	public static final String VERSION = "0.0.7.4";
+	public static final String VERSION = "0.0.7.0";
 
 	//Setting proxy for client and server side
 	@SidedProxy(clientSide = "nz.co.crookedhill.ggutils.proxy.ClientProxy", serverSide = "nz.co.crookedhill.ggutils.proxy.CommonProxy")
